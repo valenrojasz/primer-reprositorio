682d2b6d7c30180ce68cc05014702d2b464f059f
Valen Rojas <vlerojas193@gmail.com>
2024-09-08 13:22:02 -0500

    se crea la funcion contar_caracteres

--- min.py
+++ min.py
@@ -1,1 +1,8 @@
-print("Hol mundo")
+def min(num1, num2):
+    if num1 < num2:
+        return num1
+    else:
+        return num2 
+
+resultado = min(2, 5)
+print(f"El valor mínimo es {resultado}")
